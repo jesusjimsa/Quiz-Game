414497cc285735609fd5c16b2b010018bf93b6b9
jesusjimsa <jesusjimsa@icloud.com>
2017-12-11 01:26:39 +0200

    Small changes

--- src/QuizGameClient.c
+++ src/QuizGameClient.c
@@ -18,6 +18,9 @@
 #include <stdlib.h>
 #include <unistd.h>
 
+#define true (1 == 1)
+#define false (!true)
+
 /* el código de error devuelto por ciertas llamadas */
 extern int errno;
 
@@ -34,12 +37,13 @@ struct Options{
 struct Round{
 	char question[100];
 	struct Options options;
+	char correct_answer;
 };
 
-struct ResultRound{
-	char correct_answer[50];
-	int points;
-};
+// struct ResultRound{
+// 	char correct_answer[50];
+// 	int points;
+// };
 
 void printRound(struct Round ronda){
 	printf("%s\n", ronda.question);
@@ -47,9 +51,20 @@ void printRound(struct Round ronda){
 	printf("Which option is the right answer, A, B, C or D?\n");
 }
 
-void printResult(struct ResultRound result){
-	printf("The right answer was: %s\n", result.correct_answer);
-	printf("You get %d points\n", result.points);
+int printResult(struct Round ronda, int answer){
+	int points_obtained;
+	printf("The right answer was: %s\n", ronda.correct_answer);
+	
+	if(answer == true){
+		printf("You get 10 points in this round\n");
+		points_obtained = 10;
+	}
+	else{
+		printf("You get no points in this round\n");
+		points_obtained = 0;
+	}
+
+	return points_obtained;
 }
 
 int main(int argc, char *argv[]){
@@ -58,12 +73,13 @@ int main(int argc, char *argv[]){
 	char username[100];
 	struct Round ronda;
 	char answer;
-	struct ResultRound result_round;
+	//struct ResultRound result_round;
+	int points_obtained_in_round;
 	int finish = 1;	// When the server sends a -1 value to this variable, the game finishes
 
 	/* ¿Están todos los argumentos en la línea de comandos? */
 	if(argc != 3){
-      printf("Sintaxis: %s <server_name> <port>\n", argv[0]);
+      printf("Sintax: %s <server_name> <port>\n", argv[0]);
       return -1;
     }
 
@@ -77,7 +93,7 @@ int main(int argc, char *argv[]){
 
 	/* creación del socket */
 	if((sd = socket(AF_INET, SOCK_STREAM, 0)) == -1){
-		perror("Error de socket().\n");
+		perror("Error in socket().\n");
 		return errno;
     }
 
@@ -93,40 +109,45 @@ int main(int argc, char *argv[]){
 
 	/* nos conectamos al servidor */
 	if(connect(sd, (struct sockaddr *) &server, sizeof(struct sockaddr)) == -1){
-		perror("[client]Error en connect().\n");
+		perror("[client]Error in connect().\n");
 		return errno;
     }
 
 	/* enviamos el nombre de usuario al servidor para ser añadidos a la partida */
 	if(write (sd, &username, sizeof(username)) <= 0){
-		perror("[client]Error al escribir en el servidor.\n");
+		perror("[client]Error writing on the server.\n");
 		return errno;
     }
 
 	/* The game itself, questions and answers */
 	while(finish != -1){
 		if(read(sd, &ronda, sizeof(ronda)) < 0){
-			perror ("[client]Error en read() del servidor.\n");
+			perror ("[client]Error in read() from server.\n");
 			return errno;
 		}
 
 		printRound(ronda);
 		read(0, &answer, 1);
 
-		if(write (sd, &answer, 1) <= 0){
-			perror("[client]Error al escribir en el servidor.\n");
+		// if(write (sd, &answer, 1) <= 0){
+		// 	perror("[client]Error al escribir en el servidor.\n");
+		// 	return errno;
+		// }
+
+		// if(read(sd, &result_round, sizeof(result_round)) < 0){
+		// 	perror ("[client]Error en read() del servidor.\n");
+		// 	return errno;
+		// }
+
+		points_obtained_in_round = printResult(ronda, strcmp(answer, ronda.correct_answer));
+
+		if(write (sd, &points_obtained_in_round, sizeof(int)) <= 0){
+			perror("[client]Error in write() to server.\n");
 			return errno;
 		}
-
-		if(read(sd, &result_round, sizeof(result_round)) < 0){
-			perror ("[client]Error en read() del servidor.\n");
-			return errno;
-		}
-
-		printResult(result_round);
 
 		if(read(sd, &finish, sizeof(finish)) < 0){
-			perror ("[client]Error en read() del servidor.\n");
+			perror ("[client]Error in read() from server.\n");
 			return errno;
 		}
 	}
